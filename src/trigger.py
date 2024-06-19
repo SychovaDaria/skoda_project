223abba83ac914fd010ac05060ab5a628e4d65fa
kahoujo1 <kahoujo1@fel.cvut.cz>
2024-06-19 10:17:28 +0200

    tttt

--- src/trigger.py
+++ src/trigger.py
@@ -13,11 +13,12 @@ Version: 0.1
 from raspicam import Raspicam
 from typing import List
 import os
+import time
 
 # default values
 DEFAULT_TRIGGER_DELAY = 0
 DEFAULT_NUM_OF_PICTURES = 1
-DEFAULT_TIMES_BETWEEN_PICTURES = 0
+DEFAULT_TIMES_BETWEEN_PICTURES = .5
 
 class Trigger:
     """
@@ -67,8 +68,37 @@ class Trigger:
         elif isinstance(self.times_between_pictures, List[float]):
             if any(not isinstance(time, float) or time < 0 for time in self.times_between_pictures):
                 raise ValueError("The times_between_pictures attribute must be a list of non-negative floats.")
+            if len(self.times_between_pictures) != self.num_of_pictures - 1:
+                raise ValueError("The length of the times_between_pictures list must be equal to num_of_pictures - 1., or times_between_pictures must be a float.")
         if not (isinstance(self.folder_name, str) and os.path.isdir(self.folder_name)):
             raise ValueError("The folder_name attribute must be a string representing a valid directory path.")
+
+    def set_config(self,  camera : Raspicam = None, folder_name : str = None, trigger_delay : float = None,
+                   num_of_pictures: int = None, times_between_pictures : float|List[float] = None) -> None:
+        """
+        Set the configuration of the trigger.
+
+        Args:
+            camera (Raspicam, optional): Object for handling the camera controls.
+            folder_name (str, optional): The folder where the pictures will be saved.
+            trigger_delay (float, optional): The delay between the trigger and the start of the acquisition. Defaults to DEFAULT_TRIGGER_DELAY.
+            num_of_pictures (int, optional): The number of pictures to take. Defaults to DEFAULT_NUM_OF_PICTURES.
+            times_between_pictures (float|List[float], optional): The time between pictures. Can be a single float or a list of floats. Defaults to DEFAULT_TIMES_BETWEEN_PICTURES.
+
+        Returns:
+            None
+        """
+        if camera is not None:
+            self.camera = camera
+        if folder_name is not None:
+            self.folder_name = folder_name
+        if trigger_delay is not None:
+            self.trigger_delay = trigger_delay
+        if num_of_pictures is not None:
+            self.num_of_pictures = num_of_pictures
+        if times_between_pictures is not None:
+            self.times_between_pictures = times_between_pictures
+        self.check_attributes()
 
     def trigg(self) -> None:
         """
@@ -77,4 +107,18 @@ class Trigger:
         Returns:
             None
         """
-        pass
+        current_time = time.strftime("%Y%m%d_%H%M%S")
+        # wait for the trigger delay
+        time.sleep(self.trigger_delay)
+        # transform times_between_pictures to a list if it is a float
+        if isinstance(self.times_between_pictures, float):
+            times_between_pictures = [self.times_between_pictures] * (self.num_of_pictures - 1)
+        else:
+            times_between_pictures = self.times_between_pictures
+        # capture the imgs
+        for i in range(self.num_of_pictures):
+            filename = f"{current_time}_{i}"
+            self.camera.capture_img_and_save(filename=filename, folder_path=self.folder_name)
+            if i < self.num_of_pictures - 1:
+                time.sleep(times_between_pictures[i])
+        
