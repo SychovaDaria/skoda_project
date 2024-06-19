81ebcd8e47625694c55e4a19538d7fa577e550d4
SyhovaDaria <dashadf4@gmail.com>
2024-06-19 09:28:33 +0200

    Add model.ipynb to repo

--- tel/tensor.py
+++ tel/tensor.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
-#from picamera2 import Picamera2
 import time
 import os
 from raspicam import Picamera2
@@ -12,12 +11,8 @@ model = load_model('phone_detector_model.h5')
 # params
 img_height, img_width = 150, 150
 
-# cam initializace
-# picam2 = Picamera2()
-# camera_config = picam2.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"})
-# picam2.configure(camera_config)
-# picam2.start()
-camera = Paspicam()
+# Initialize camera
+camera = Picamera2()
 
 def preprocess_image(image):
     image = cv2.resize(image, (img_height, img_width))
@@ -35,14 +30,19 @@ os.makedirs('mobil', exist_ok=True)
 while True:
     frame = camera.capture_img()
     if detect_phone(frame):
-        timestamp = time.strftime("%Y%m%d-%H%M%S")
-        filename = os.path.join('mobil', f'phone_detected_{timestamp}.jpg')
-        cv2.imwrite(filename, frame)
-        print(f'Phone detected! Saved {filename}')
+        for _ in range(3):
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            filename = os.path.join('mobil', f'phone_detected_{timestamp}.jpg')
+            cv2.imwrite(filename, frame)
+            print(f'Phone detected! Saved {filename}')
+            time.sleep(0.5)  # Delay between captures
+        time.sleep(20)  # Wait 20 seconds before next detection attempt
+    else:
+        time.sleep(0.5)  # Check for phone every 0.5 seconds if not detected
 
     cv2.imshow('Camera Stream', frame)
 
-    if cv2.waitKey(1) == 27:  #esc 
+    if cv2.waitKey(1) == 27:  # Escape key
         break
 
 camera.stop()
