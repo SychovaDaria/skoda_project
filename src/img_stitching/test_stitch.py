6508316b5b9bcca4c8dab4724373d142cb7de6cf
kahoujo1 <kahoujo1@fel.cvut.cz>
2024-07-24 07:38:28 +0200

    moved gui to folder

--- src/img_stitching/test_stitch.py
+++ src/img_stitching/test_stitch.py
@@ -6,15 +6,15 @@ import numpy as np
 from stitcher import Stitcher
 
 def main():
-    img1 = cv2.imread("../../test_img/stitching/img4.jpeg")
-    img2 = cv2.imread("../../test_img/stitching/img5.jpeg")
+    img1 = cv2.imread("../../test_img/stitching/img1.jpeg")
+    img2 = cv2.imread("../../test_img/stitching/img2.jpeg")
     img3 = cv2.imread("../../test_img/stitching/img3.jpeg")
     stitcher = Stitcher()
-    stat, img = stitcher.stitch_images([img1,img2])#stitcher.stitch_images([img1, img2])
+    stat, img = stitcher.stitch_images([img1,img2,img3])#stitcher.stitch_images([img1, img2])
     if stat == 0:
         print("Stitched successfully")
         cv2.imwrite("../../test_img/stitching/stitched.jpeg", img)
     print("yupiii")
-
+ 
 if __name__ == "__main__":
     main()
